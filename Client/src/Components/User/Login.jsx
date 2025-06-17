186463f62be70da812ad3f977751659953402653
Abdelrahman <abdotrika351@gmail.com>
2025-06-17 05:36:01 +0300

    Try to connect back-end

--- Client/src/Components/User/Login.jsx
+++ Client/src/Components/User/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('http://localhost:8000/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
